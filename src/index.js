c4a74a1f828746523bb07e0907ae6121137f571c
Inbar Vernia <inbar.vernia@gmail.com>
2022-02-02 22:49:03 +0000

    Rendering paddle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import Paddle from "./paddle";
+import Paddle from "./paddle.js"; // remember to include the file extension in the import statement - that's why it wasn't working before!
 
 const canvas = document.getElementById('gameScreen');
-const ctx = canvas.getContext('2d'); // ctx is a common abbreviation for context; the getContext method returns a drawing context, i.e. the way things are rendered on the canvas, such as 2d, or webGL for 3D objects
+let ctx = canvas.getContext('2d'); // ctx is a common abbreviation for context; the getContext method returns a drawing context, i.e. the way things are rendered on the canvas, such as 2d, or webGL for 3D objects
 
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600; // declaring these in order to be able to pass them as arguments when constructing the paddle object
+
 
 ctx.clearRect(0, 0, canvas.width, canvas.height); // According to the tutorial, this is necessary to clear the canvas so that shapes don't seem to multiply if you change their coordinates and save, but I have not found that to happen before adding this line - could be a difference between Sandbox Editor and VSCode Live Server
 // According to MDN documentation, erasing the whole canvas in this way is commonly used at the start of each frame in an animation  (https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clearRect)
@@ -12,6 +13,3 @@ ctx.clearRect(0, 0, canvas.width, canvas.height); // According to the tutorial, 
 const paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 
 paddle.draw(ctx);
-
-
-ctx.fillRect(85, 560, 150, 30);
