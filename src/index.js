5fb7c49ba07586754902badbbb8c7d8f749cb97c
Inbar Vernia <inbar.vernia@gmail.com>
2022-02-02 21:33:27 +0000

    Attempted to render paddle according to tutorial but for some reason not working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import Paddle from "./paddle";
+
 const canvas = document.getElementById('gameScreen');
 const ctx = canvas.getContext('2d'); // ctx is a common abbreviation for context; the getContext method returns a drawing context, i.e. the way things are rendered on the canvas, such as 2d, or webGL for 3D objects
 
@@ -6,3 +8,10 @@ const GAME_HEIGHT = 600; // declaring these in order to be able to pass them as 
 
 ctx.clearRect(0, 0, canvas.width, canvas.height); // According to the tutorial, this is necessary to clear the canvas so that shapes don't seem to multiply if you change their coordinates and save, but I have not found that to happen before adding this line - could be a difference between Sandbox Editor and VSCode Live Server
 // According to MDN documentation, erasing the whole canvas in this way is commonly used at the start of each frame in an animation  (https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clearRect)
+
+const paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+paddle.draw(ctx);
+
+
+ctx.fillRect(85, 560, 150, 30);
