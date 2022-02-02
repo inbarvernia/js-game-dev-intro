f4f35e7676a205239e6e1310d3f4195231c5bbc3
Inbar Vernia <inbar.vernia@gmail.com>
2022-02-02 22:55:46 +0000

    Tweaked paddle appearance

--- src/paddle.js
+++ src/paddle.js
@@ -1,15 +1,16 @@
 export default class Paddle {
   constructor(gameWidth, gameHeight) {
-    this.width = 150;
-    this.height = 30;
+    this.width = 130;
+    this.height = 20;
 
     this.position = {
-      x: (gameWidth - this.width) / 2,
+      x: (gameWidth - this.width) / 2, // Placing the paddle in the missle of the x axis by setting its starting point (left edge) to halfway through the canvas minus half the width of the paddle
       y: gameHeight - this.height - 10 // Placing the paddle 10px up from the bottom of the canvas
     }
   }
 
   draw(ctx) { // We will use the context variable from index.js as an argument for this function
+    ctx.fillStyle = '#0099ee';
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
   }
 }
