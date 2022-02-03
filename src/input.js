997e00eaf3e1affca92ed21bb4415d380bfce655
Inbar Vernia <inbar.vernia@gmail.com>
2022-02-03 16:11:10 +0000

    Added moveLeft method to paddle, corrected syntax in input handler (missing quote marks around key strings)

--- src/input.js
+++ src/input.js
@@ -3,10 +3,10 @@ export default class InputHandler {
   constructor() {
     document.addEventListener('keydown', (event) => { // This function listens for a key press
       switch(event.key) { // The KeyboardEvent interface's key read-only property returns the value of the key being pressed; see: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
-        case ArrowLeft:
+        case "ArrowLeft":
           alert('move left');
           break;
-        case ArrowRight:
+        case "ArrowRight":
           alert('move right');
           break;
       }
--- src/paddle.js
+++ src/paddle.js
@@ -3,10 +3,17 @@ export default class Paddle {
     this.width = 130;
     this.height = 20;
 
+    this.maxSpeed = 7;
+    this.speed = 0; // Default speed is 0 because the paddle should remain stationary until moved by player
+
     this.position = {
       x: (gameWidth - this.width) / 2, // Placing the paddle in the missle of the x axis by setting its starting point (left edge) to halfway through the canvas minus half the width of the paddle
       y: gameHeight - this.height - 10 // Placing the paddle 10px up from the bottom of the canvas
     }
+  }
+
+  moveLeft() {
+    this.speed = - this.maxSpeed; // Negative speed because it's moving to the left, i.e. lower on the x axis
   }
 
   draw(ctx) { // We will use the context variable from index.js as an argument for this function
@@ -14,9 +21,9 @@ export default class Paddle {
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
   }
 
-  update(deltaTime) { // delta time is often abbreviated dt, and is often used in games to refer to change in time
+  update(deltaTime) { // Delta time is often abbreviated dt, and is often used in games to refer to change in time
     if (!deltaTime) return; // Because delta time will be 0 for the first frame, and attempting to divide by 0 will cause problems, so instead we just return without moving the paddle
 
-    this.position.x += 5 / deltaTime;
+    this.position.x += this.speed; // Every delta time (i.e. every frame), the paddle will move depending on the speed it currently has, whether it's staitionary (0), moving left (- this.maxSpeed) or moving right (+ this.maxSpeed)
   }
 }
