b075b4d4dcbf42bae5b75771254f8dece11e0e6c
Inbar Vernia <inbar.vernia@gmail.com>
2022-02-03 11:44:09 +0000

    Added switch statement in input handler to respond to left and right arrow key presses

--- src/input.js
+++ src/input.js
@@ -1,8 +1,16 @@
 export default class InputHandler {
 
   constructor() {
-    document.addEventListener('keydown', (event) => {
-      alert(event.keyCode); // This function listens for a key press and creates an alert with the code (number) for the key pressed); keyCode is deprecated - will look for an alternateive when refactoring
+    document.addEventListener('keydown', (event) => { // This function listens for a key press
+      // keyCode returns the code (number) for the key pressed; however, it is deprecated, so will look for an alternateive when refactoring
+      switch(event.keyCode) {
+        case 37: // left arrow key code
+          alert('move left');
+          break;
+        case 39: // right arrow key code
+          alert('move right');
+          break;
+      }
     })
   }
 
