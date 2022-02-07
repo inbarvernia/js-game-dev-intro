8fe1e577d24625c1ce4dff9406b7037a969060ed
Inbar Vernia <inbar.vernia@gmail.com>
2022-02-07 20:44:09 +0000

    Created Game start function

--- src/game.js
+++ src/game.js
@@ -1,7 +1,18 @@
+import Ball from "./ball";
+import InputHandler from "./input";
+import Paddle from "./paddle";
+
 export default class Game {
 
   constructor(gameWidth, gameHeight) {
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
+  }
+
+  start() {
+    const ball = new Ball(this);
+    const paddle = new Paddle(this);
+
+    new InputHandler(paddle);
   }
 }
