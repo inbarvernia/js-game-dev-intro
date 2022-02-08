310d42cfe640226da4666f4cb9782f6027b83591
Inbar Vernia <inbar.vernia@gmail.com>
2022-02-08 12:43:34 +0000

    Deleted commented out code from index.js

--- src/index.js
+++ src/index.js
@@ -10,9 +10,6 @@ const GAME_HEIGHT = 600; // declaring these in order to be able to pass them as 
 ctx.clearRect(0, 0, canvas.width, canvas.height); // According to the tutorial, this is necessary to clear the canvas so that shapes don't seem to multiply if you change their coordinates and save, but I have not found that to happen before adding this line - could be a difference between Sandbox Editor and VSCode Live Server
 // According to MDN documentation, erasing the whole canvas in this way is commonly used at the start of each frame in an animation  (https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clearRect)
 
-// const paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-// const ball = new Ball(GAME_WIDTH, GAME_HEIGHT);
-
 const game = new Game(GAME_WIDTH,GAME_HEIGHT);
 game.start();
 
