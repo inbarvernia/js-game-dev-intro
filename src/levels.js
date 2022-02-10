68d4073983b01d85de5d8393ec86daadbe42f158
Inbar Vernia <inbar.vernia@gmail.com>
2022-02-10 23:34:33 +0000

    Created level 1 brick placement pattern

---
+++ src/levels.js
@@ -0,0 +1,6 @@
+export const level1 = [
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]; // This array will represent the placement of bricks for this level of the game: each sub array will be a row of bricks; 1 represents a brick, and 0 represents a space between bricks
