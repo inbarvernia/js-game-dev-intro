3f7efc083fa878c2d645cc5b955d8171bd75f32f
Inbar Vernia <inbar.vernia@gmail.com>
2022-02-10 23:19:49 +0000

    Created constructor function for Brick class

---
+++ src/brick.js
@@ -0,0 +1,15 @@
+export default class Brick {
+
+  constructor(game, position) {
+    this.image = document.getElementById('brick_img');
+
+    this.position = position;
+
+    this.width = 52;
+    this.height = 24;
+
+    this.game = game; // Gives other methods in this class access to the game object, so that we can use it to refer to other game objects (paddle and bricks)
+    this.gameWidth = game.gameWidth;
+    this.gameHeight = game.gameHeight;
+  }
+}
