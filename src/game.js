2d2bf28cbedc9ab3058b39f252d479f11f198949
Inbar Vernia <inbar.vernia@gmail.com>
2022-02-10 23:23:23 +0000

    Added brick to Game start function

--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import Ball from "./ball.js";
 import InputHandler from "./input.js";
 import Paddle from "./paddle.js";
+import Brick from "./brick.js";
 
 export default class Game {
 
@@ -12,8 +13,9 @@ export default class Game {
   start() {
     this.ball = new Ball(this);
     this.paddle = new Paddle(this);
+    this.brick = new Brick(this, {x: 20, y: 20});
 
-    this.gameObject = [this.ball, this.paddle] // Created this array for ease of use in update and draw functions: this way, even as we add more objects to the game (e.g. bricks), we don't need to keep adding object.update(deltaTime) to the update function below
+    this.gameObject = [this.ball, this.paddle, this.brick] // Created this array for ease of use in update and draw functions: this way, even as we add more objects to the game (e.g. bricks), we don't need to keep adding object.update(deltaTime) to the update function below
 
     new InputHandler(this.paddle);
   }
